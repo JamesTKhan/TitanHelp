53b856b270e349c5db62f1bc74349b77db6eda8d
James <wjamesfl@gmail.com>
2024-09-23 16:54:11 -0400

    Add initial Ticket Model class

---
+++ TitanHelp/Domain/Ticket.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TitanHelp.Domain
+{
+    public class Ticket
+    {
+        public int Id { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime Date { get; set; } = DateTime.Now;
+
+        public string ClientName { get; set; } = string.Empty;
+
+        public string Description { get; set; } = string.Empty;
+    }
+}
