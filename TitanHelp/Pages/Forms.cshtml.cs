ec40467ef8defd1fff4bb1337f8f570e2a119cd6
Nehemiah-J <nehmijames@outlook.com>
2024-09-25 20:08:34 -0400

    Added test ticket to save function (test later)

--- TitanHelp/Pages/Forms.cshtml.cs
+++ TitanHelp/Pages/Forms.cshtml.cs
@@ -1,25 +1,36 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using TitanHelp.Data;
+using TitanHelp.Models;
 
 namespace TitanHelp.Pages
 {
     public class FormsModel : PageModel
     {
         private readonly ILogger<FormsModel> _logger;
+        private readonly ITicketDb _ticketDb;
 
-        public FormsModel(ILogger<FormsModel> logger)
+        public FormsModel(ILogger<FormsModel> logger, ITicketDb ticketDb)
         {
             _logger = logger;
+            _ticketDb = ticketDb;   
         }
 
         public void OnGet()
         {
         }
 
-        protected void save_Click(object sender, EventArgs e)
+        public void save_Click(object sender, EventArgs e)
         {
             // Later this function will need to read 3? (2 if date is automatic) fields from the UI to create a new ticket object
-            
+
+            var ticket = new Ticket
+            {
+                ClientName = "James",
+                Description = "This is a test ticket"
+            };
+
+            _ticketDb.SaveTicket(ticket);
 
         }
     }
