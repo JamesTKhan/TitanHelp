e0b6de54d0ad8398d3908feb07df9b3b517867e0
Nehemiah-J <nehmijames@outlook.com>
2024-09-30 12:56:31 -0400

    Connect To Database to Add and Display Tickets

--- TitanHelp/Pages/Forms.cshtml.cs
+++ TitanHelp/Pages/Forms.cshtml.cs
@@ -22,21 +22,24 @@ namespace TitanHelp.Pages
         {
         }
 
-        public void save_Click(object sender, EventArgs e)
+        public void OnPost()
         {
             // Later this function will need to read 3? (2 if date is automatic) fields from the UI to create a new ticket object
 
             string Name = Request.Form["name"];
             string Date = Request.Form["date"];
-            string Desc = Request.Form["description"];
+            string Desc = Request.Form["description"]; 
 
             var ticket = new Ticket
             {
                 ClientName = Name,
+                Date = DateTime.Parse(Date),
                 Description = Desc
             };
 
             _ticketDb.SaveTicket(ticket);
+
+            Response.Redirect("/main");
 
         }
     }
